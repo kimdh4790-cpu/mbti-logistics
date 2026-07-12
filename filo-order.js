a728bd9ad79a400604e707557be8f366b26048fd
kimdh4790-cpu <kimdh4790@gmail.com>
2026-07-12 22:33:10 +0900

    fix: _renderOrders snap undefined 에러 수정

--- filo-order.js
+++ filo-order.js
@@ -108,9 +108,11 @@ function _filoPageOrders(el){
       '</div></div>';
     }).join('');
    // 새 주문 알림음
-   if(snap.docChanges().some(function(c){return c.type==='added'&&c.doc.data().status!=='done';})){
-    try{var a=new AudioContext();var o2=a.createOscillator();o2.connect(a.destination);o2.frequency.value=880;o2.start();o2.stop(a.currentTime+0.15);}catch(e){}
+    // 새 QR 주문 알림음
+    if(window._filoLastOrderCount!==undefined&&_oQR.length>window._filoLastOrderCount){
    }
+    }
+    window._filoLastOrderCount=_oQR.length;
   }
  var _u1=_db.collection('filo_sales')
   .where('dealerId','==',did).where('date','==',today)
