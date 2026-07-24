75af54f882183ba802310ec4b1ad58e4bcd0ba52
kimdh4790-cpu <kimdh4790@gmail.com>
2026-07-24 17:56:46 +0900

    fix: menu_mgmt _filoLoadAndRun으로 수정

--- filo-auth.js
+++ filo-auth.js
@@ -416,7 +416,9 @@ function _filoGoPage(p){
 
  if(p==='home') _filoPageHome(el);
  else if(p==='kiosk') _filoPageKiosk(el);
- else if(p==='menu_mgmt') _filoPageMenuMgmt(el);
+ else if(p==='menu_mgmt') {
+  _filoLoadAndRun('filo-menu-mgmt.js', function(){ _filoPageMenuMgmt(el); });
+ }
  else if(p==='qr_mgmt') {
   _filoLoadAndRun('filo-menu-mgmt.js', function(){ _filoPageQrMgmt(el); });
  }
